1961b4173d755abb00fd4db5cf219ff1b4f496ab
Qin Dong Liang <182100507492@126.com>
2018-03-29 00:06:01 +0800

    my blog

---
+++ _site/README.md
@@ -0,0 +1,79 @@
+# 地址
+
+ [我的Blog预览](http://blog.tmaize.net/) 欢迎添加友链
+
+ [github 地址](https://github.com/TMaize/tmaize-blog) 欢迎star
+
+# 说明
+
+一款jekyll主题，简洁纯净，支持自适应
+
+由于一直在这个项目里面修改主题代码，和以前的项目simple-jekyll-theme差异太大，现在把simple-jekyll-theme删除了，只留这一个项目,只有一个master分支
+
+ 
+# 截图
+
+![s1](readme/01.jpg)
+
+![s2](readme/02.jpg)
+
+# 使用
+
+## 配置说明
+
+```
+encoding: utf-8
+
+# seo
+title: TMaize'Blog
+description: TMaize'Blog
+keywords: TMaize,Blog,TMaize'Blog
+author: TMaize
+
+# 上下文环境，"","/blog"
+context: ""
+scheme: "http"
+domain: "blog.tmaize.net"
+
+## 文章url前缀
+permalink: /posts/:year/:month/:day/:title.html
+
+copyright: 2016
+beianhao: "皖ICP备16016174号"
+
+coderay:
+  coderay_tab_width: 4
+```
+
+
+## 写文章
+
+符合 jekyll 的使用规范，请参考我的文件放置规则
+
+文章放在_posts目录
+
+文章资源放在posts目录
+
+## 要修改的文件
+
++ _includes/footer.html
+
+    请删除本页所有的script标签内的东西
+
+    或者把百度和360的seo推送,mta统计代码替换成你自己的
+
++ pages/chat.html
+
+    请换成别的评论插件
+    
+    比如:
+
+    [https://github.com/imsun/gitment](https://github.com/imsun/gitment)
+
+    [https://github.com/gitalk/gitalk](https://github.com/gitalk/gitalk)
+
++ CNAME
+
+    如果支持cname请换成你自己的域名
+
+
---
+++ _site/static/js/search.js
@@ -0,0 +1,704 @@
+function search() {
+var data = [
+
+{
+"title" : "如何在elasticsearch里面使用深度分页功能",
+"time" : "2018/03/28",
+"categories":"elasticsearch",
+"url" : "/posts/2018/03/28/%E5%A6%82%E4%BD%95%E5%9C%A8elasticsearch%E9%87%8C%E9%9D%A2%E4%BD%BF%E7%94%A8%E6%B7%B1%E5%BA%A6%E5%88%86%E9%A1%B5%E5%8A%9F%E8%83%BD.html"
+}
+
+,
+
+
+{
+"title" : "SSO单点登陆",
+"time" : "2018/03/14",
+"categories":"系统设计",
+"url" : "/posts/2018/03/14/sso%E5%8D%95%E7%82%B9%E7%99%BB%E9%99%86.html"
+}
+
+,
+
+
+{
+"title" : "Git&Hook实现自动部署",
+"time" : "2018/03/10",
+"categories":"小技巧",
+"url" : "/posts/2018/03/10/Git-Hook%E5%AE%9E%E7%8E%B0%E8%87%AA%E5%8A%A8%E9%83%A8%E7%BD%B2.html"
+}
+
+,
+
+
+{
+"title" : "自己实现Object转Json",
+"time" : "2018/03/04",
+"categories":"java",
+"url" : "/posts/2018/03/04/%E8%87%AA%E5%B7%B1%E5%AE%9E%E7%8E%B0Object%E8%BD%ACJson.html"
+}
+
+,
+
+
+{
+"title" : "CentOS下搭建Aria2",
+"time" : "2018/02/23",
+"categories":"linux",
+"url" : "/posts/2018/02/23/CentOS%E4%B8%8B%E6%90%AD%E5%BB%BAAria2.html"
+}
+
+,
+
+
+{
+"title" : "Combo Handler 简单实现",
+"time" : "2018/02/08",
+"categories":"java",
+"url" : "/posts/2018/02/08/combo-handler-%E7%AE%80%E5%8D%95%E5%AE%9E%E7%8E%B0.html"
+}
+
+,
+
+
+{
+"title" : "重写博客留言板",
+"time" : "2018/01/29",
+"categories":"jekyll",
+"url" : "/posts/2018/01/29/%E9%87%8D%E5%86%99%E5%8D%9A%E5%AE%A2%E7%95%99%E8%A8%80%E6%9D%BF.html"
+}
+
+,
+
+
+{
+"title" : "解决GitPage百度不收录",
+"time" : "2018/01/21",
+"categories":"seo",
+"url" : "/posts/2018/01/21/%E8%A7%A3%E5%86%B3GitPage%E7%99%BE%E5%BA%A6%E4%B8%8D%E6%94%B6%E5%BD%95.html"
+}
+
+,
+
+
+{
+"title" : "Vue数据绑定的原理",
+"time" : "2018/01/07",
+"categories":"javascript",
+"url" : "/posts/2018/01/07/vue%E6%95%B0%E6%8D%AE%E7%BB%91%E5%AE%9A%E7%9A%84%E5%8E%9F%E7%90%86.html"
+}
+
+,
+
+
+{
+"title" : "File Img Canvas的操作总结",
+"time" : "2017/12/10",
+"categories":"javascript",
+"url" : "/posts/2017/12/10/file-img-canvas%E7%9A%84%E6%93%8D%E4%BD%9C%E6%80%BB%E7%BB%93.html"
+}
+
+,
+
+
+{
+"title" : "移动端拖动事件的坑",
+"time" : "2017/11/21",
+"categories":"javascript",
+"url" : "/posts/2017/11/21/%E7%A7%BB%E5%8A%A8%E7%AB%AF%E6%8B%96%E5%8A%A8%E4%BA%8B%E4%BB%B6%E7%9A%84%E5%9D%91.html"
+}
+
+,
+
+
+{
+"title" : "JS的事件委托",
+"time" : "2017/11/21",
+"categories":"javascript",
+"url" : "/posts/2017/11/21/JS%E7%9A%84%E4%BA%8B%E4%BB%B6%E5%A7%94%E6%89%98.html"
+}
+
+,
+
+
+{
+"title" : "ReqProxy",
+"time" : "2017/11/12",
+"categories":"java",
+"url" : "/posts/2017/11/12/ReqProxy.html"
+}
+
+,
+
+
+{
+"title" : "NodeJs 上手",
+"time" : "2017/10/14",
+"categories":"nodejs",
+"url" : "/posts/2017/10/14/nodejs%E4%B8%8A%E6%89%8B.html"
+}
+
+,
+
+
+{
+"title" : "Centos 安装NodeJs",
+"time" : "2017/10/07",
+"categories":"linux@nodejs",
+"url" : "/posts/2017/10/07/centos-install-nodejs.html"
+}
+
+,
+
+
+{
+"title" : "Git日常使用",
+"time" : "2017/09/20",
+"categories":"小技巧",
+"url" : "/posts/2017/09/20/git%E6%97%A5%E5%B8%B8%E4%BD%BF%E7%94%A8.html"
+}
+
+,
+
+
+{
+"title" : "JUnit的正确使用姿势",
+"time" : "2017/09/14",
+"categories":"java",
+"url" : "/posts/2017/09/14/junit%E7%9A%84%E6%AD%A3%E7%A1%AE%E4%BD%BF%E7%94%A8%E5%A7%BF%E5%8A%BF.html"
+}
+
+,
+
+
+{
+"title" : "原生js插件封装",
+"time" : "2017/08/15",
+"categories":"javascript",
+"url" : "/posts/2017/08/15/%E5%8E%9F%E7%94%9Fjs%E6%8F%92%E4%BB%B6%E5%B0%81%E8%A3%85.html"
+}
+
+,
+
+
+{
+"title" : "正确引入Layui模块",
+"time" : "2017/08/09",
+"categories":"javascript",
+"url" : "/posts/2017/08/09/%E6%AD%A3%E7%A1%AE%E5%BC%95%E5%85%A5layui%E6%A8%A1%E5%9D%97.html"
+}
+
+,
+
+
+{
+"title" : "Start With Spring Boot",
+"time" : "2017/08/08",
+"categories":"java",
+"url" : "/posts/2017/08/08/start-with-spring-boot.html"
+}
+
+,
+
+
+{
+"title" : "用maven构建项目",
+"time" : "2017/08/04",
+"categories":"java",
+"url" : "/posts/2017/08/04/%E7%94%A8maven%E6%9E%84%E5%BB%BA%E9%A1%B9%E7%9B%AE.html"
+}
+
+,
+
+
+{
+"title" : "对MVP架构的一些理解",
+"time" : "2017/08/02",
+"categories":"android",
+"url" : "/posts/2017/08/02/%E5%AF%B9MVP%E6%9E%B6%E6%9E%84%E7%9A%84%E4%B8%80%E4%BA%9B%E7%90%86%E8%A7%A3.html"
+}
+
+,
+
+
+{
+"title" : "GitHub配置SSH登陆",
+"time" : "2017/07/22",
+"categories":"小技巧",
+"url" : "/posts/2017/07/22/github%E9%85%8D%E7%BD%AEssh%E7%99%BB%E9%99%86.html"
+}
+
+,
+
+
+{
+"title" : "面向对象与原型",
+"time" : "2017/07/19",
+"categories":"javascript",
+"url" : "/posts/2017/07/19/%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E4%B8%8E%E5%8E%9F%E5%9E%8B.html"
+}
+
+,
+
+
+{
+"title" : "页面无刷新的两种实现",
+"time" : "2017/07/13",
+"categories":"javascript",
+"url" : "/posts/2017/07/13/%E9%A1%B5%E9%9D%A2%E6%97%A0%E5%88%B7%E6%96%B0%E7%9A%84%E4%B8%A4%E7%A7%8D%E5%AE%9E%E7%8E%B0.html"
+}
+
+,
+
+
+{
+"title" : "wamp环境配置及拓展",
+"time" : "2017/07/10",
+"categories":"php",
+"url" : "/posts/2017/07/10/wamp%E7%8E%AF%E5%A2%83%E9%85%8D%E7%BD%AE%E5%8F%8A%E6%8B%93%E5%B1%95.html"
+}
+
+,
+
+
+{
+"title" : "校园网竟然支持ipv6",
+"time" : "2017/06/25",
+"categories":"小技巧",
+"url" : "/posts/2017/06/25/%E6%A0%A1%E5%9B%AD%E7%BD%91%E7%AB%9F%E7%84%B6%E6%94%AF%E6%8C%81ipv6.html"
+}
+
+,
+
+
+{
+"title" : "SpringMVC 常见问题",
+"time" : "2017/06/24",
+"categories":"java",
+"url" : "/posts/2017/06/24/spring-mvc%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98.html"
+}
+
+,
+
+
+{
+"title" : "SpringMVC 注解 数据传递 拦截器",
+"time" : "2017/06/15",
+"categories":"java",
+"url" : "/posts/2017/06/15/spring-mvc-%E6%B3%A8%E8%A7%A3-%E6%95%B0%E6%8D%AE%E4%BC%A0%E9%80%92-%E6%8B%A6%E6%88%AA%E5%99%A8.html"
+}
+
+,
+
+
+{
+"title" : "SpringMVC 运行流程及基本配置代码",
+"time" : "2017/06/14",
+"categories":"java",
+"url" : "/posts/2017/06/14/spring-mvc-%E8%BF%90%E8%A1%8C%E6%B5%81%E7%A8%8B-%E7%AE%80%E5%8D%95%E9%85%8D%E7%BD%AE.html"
+}
+
+,
+
+
+{
+"title" : "学校图书馆登陆模拟",
+"time" : "2017/06/13",
+"categories":"java@小技巧",
+"url" : "/posts/2017/06/13/%E5%AD%A6%E6%A0%A1%E5%9B%BE%E4%B9%A6%E9%A6%86%E7%99%BB%E9%99%86%E6%A8%A1%E6%8B%9F.html"
+}
+
+,
+
+
+{
+"title" : "SSH框架搭建",
+"time" : "2017/06/06",
+"categories":"java",
+"url" : "/posts/2017/06/06/ssh%E6%A1%86%E6%9E%B6%E6%90%AD%E5%BB%BA.html"
+}
+
+,
+
+
+{
+"title" : "Android中的各种弹窗实现",
+"time" : "2017/04/30",
+"categories":"android",
+"url" : "/posts/2017/04/30/android%E4%B8%AD%E7%9A%84%E5%90%84%E7%A7%8D%E5%BC%B9%E7%AA%97%E5%AE%9E%E7%8E%B0.html"
+}
+
+,
+
+
+{
+"title" : "Nginx配置SSL 重新编译",
+"time" : "2017/04/19",
+"categories":"linux",
+"url" : "/posts/2017/04/19/nginx%E9%85%8D%E7%BD%AEssl-%E9%87%8D%E6%96%B0%E7%BC%96%E8%AF%91.html"
+}
+
+,
+
+
+{
+"title" : "Hibernate学习笔记03 数据库操作 检索 表关系",
+"time" : "2017/04/08",
+"categories":"java",
+"url" : "/posts/2017/04/08/hibernate%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B003-%E6%95%B0%E6%8D%AE%E5%BA%93%E6%93%8D%E4%BD%9C-%E6%A3%80%E7%B4%A2-%E8%A1%A8%E5%85%B3%E7%B3%BB.html"
+}
+
+,
+
+
+{
+"title" : "Hibernate学习笔记02 实体类 主键生成 缓存 事务 session绑定",
+"time" : "2017/04/06",
+"categories":"java",
+"url" : "/posts/2017/04/06/hibernate%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B002-%E5%AE%9E%E4%BD%93%E7%B1%BB-%E4%B8%BB%E9%94%AE-%E7%BC%93%E5%AD%98-%E4%BA%8B%E5%8A%A1-session%E7%BB%91%E5%AE%9A.html"
+}
+
+,
+
+
+{
+"title" : "Hibernate学习笔记01 环境配置 第一个Demo",
+"time" : "2017/04/04",
+"categories":"java",
+"url" : "/posts/2017/04/04/hibernate%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B001-%E7%8E%AF%E5%A2%83%E9%85%8D%E7%BD%AE-%E7%AC%AC%E4%B8%80%E4%B8%AAdemo.html"
+}
+
+,
+
+
+{
+"title" : "Struts2学习-05 拦截器 自带的标签库",
+"time" : "2017/04/01",
+"categories":"java",
+"url" : "/posts/2017/04/01/struts2%E5%AD%A6%E4%B9%A005-%E8%BF%87%E6%BB%A4%E5%99%A8-%E6%A0%87%E7%AD%BE.html"
+}
+
+,
+
+
+{
+"title" : "Struts2学习-04 OGNL 值栈",
+"time" : "2017/03/25",
+"categories":"java",
+"url" : "/posts/2017/03/25/struts2%E5%AD%A6%E4%B9%A004-ognl-%E5%80%BC%E6%A0%88.html"
+}
+
+,
+
+
+{
+"title" : "Struts2学习-03 页面配置 获取数据",
+"time" : "2017/03/23",
+"categories":"java",
+"url" : "/posts/2017/03/23/struts2%E5%AD%A6%E4%B9%A003-%E9%A1%B5%E9%9D%A2%E9%85%8D%E7%BD%AE-%E8%8E%B7%E5%8F%96%E6%95%B0%E6%8D%AE.html"
+}
+
+,
+
+
+{
+"title" : "修改主题的留言模块为畅言",
+"time" : "2017/03/22",
+"categories":"jekyll",
+"url" : "/posts/2017/03/22/%E4%BF%AE%E6%94%B9%E4%B8%BB%E9%A2%98%E7%9A%84%E7%95%99%E8%A8%80%E6%A8%A1%E5%9D%97%E4%B8%BA%E7%95%85%E8%A8%80.html"
+}
+
+,
+
+
+{
+"title" : "Struts2学习-02 配置 注解 Action",
+"time" : "2017/03/21",
+"categories":"java",
+"url" : "/posts/2017/03/21/struts2%E5%AD%A6%E4%B9%A002-%E9%85%8D%E7%BD%AE-%E6%B3%A8%E8%A7%A3-action.html"
+}
+
+,
+
+
+{
+"title" : "android 自定义view/dialog-九宫格解锁",
+"time" : "2017/03/19",
+"categories":"android",
+"url" : "/posts/2017/03/19/android-%E8%87%AA%E5%AE%9A%E4%B9%89view-dialog-%E4%B9%9D%E5%AE%AB%E6%A0%BC%E8%A7%A3%E9%94%81.html"
+}
+
+,
+
+
+{
+"title" : "Visual Studio Code 自定义用户代码片段",
+"time" : "2017/03/16",
+"categories":"小技巧",
+"url" : "/posts/2017/03/16/vscode-%E8%87%AA%E5%AE%9A%E4%B9%89%E7%94%A8%E6%88%B7%E4%BB%A3%E7%A0%81%E7%89%87%E6%AE%B5.html"
+}
+
+,
+
+
+{
+"title" : "Struts2学习-01 First Demo",
+"time" : "2017/03/12",
+"categories":"java",
+"url" : "/posts/2017/03/12/struts2%E5%AD%A6%E4%B9%A001-first-demo.html"
+}
+
+,
+
+
+{
+"title" : "DAO开发模式",
+"time" : "2017/02/14",
+"categories":"java",
+"url" : "/posts/2017/02/14/dao%E5%BC%80%E5%8F%91%E6%A8%A1%E5%BC%8F.html"
+}
+
+,
+
+
+{
+"title" : "SQL常用语句",
+"time" : "2016/11/22",
+"categories":"sql",
+"url" : "/posts/2016/11/22/sql%E5%B8%B8%E7%94%A8%E8%AF%AD%E5%8F%A5.html"
+}
+
+,
+
+
+{
+"title" : "MyBatis笔记整理",
+"time" : "2016/11/17",
+"categories":"java",
+"url" : "/posts/2016/11/17/mybatis%E7%AC%94%E8%AE%B0%E6%95%B4%E7%90%86.html"
+}
+
+,
+
+
+{
+"title" : "Servlet3.0的AsyncContext",
+"time" : "2016/10/25",
+"categories":"java",
+"url" : "/posts/2016/10/25/servlet3-0-asynccontext.html"
+}
+
+,
+
+
+{
+"title" : "使用CSS3让网页动起来",
+"time" : "2016/10/15",
+"categories":"html/css",
+"url" : "/posts/2016/10/15/%E4%BD%BF%E7%94%A8CSS3%E8%AE%A9%E7%BD%91%E9%A1%B5%E5%8A%A8%E8%B5%B7%E6%9D%A5.html"
+}
+
+,
+
+
+{
+"title" : "Java微信开发基本配置",
+"time" : "2016/08/30",
+"categories":"java",
+"url" : "/posts/2016/08/30/java-%E5%BE%AE%E4%BF%A1%E5%BC%80%E5%8F%91%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE.html"
+}
+
+,
+
+
+{
+"title" : "CSS3.0 变换 渐变 动画",
+"time" : "2016/08/20",
+"categories":"html/css",
+"url" : "/posts/2016/08/20/css3-%E5%8F%98%E6%8D%A2-%E6%B8%90%E5%8F%98-%E5%8A%A8%E7%94%BB.html"
+}
+
+,
+
+
+{
+"title" : "观察者模式 单例模式",
+"time" : "2016/06/12",
+"categories":"设计模式",
+"url" : "/posts/2016/06/12/%E8%A7%82%E5%AF%9F%E8%80%85%E6%A8%A1%E5%BC%8F-%E5%8D%95%E4%BE%8B%E6%A8%A1%E5%BC%8F.html"
+}
+
+,
+
+
+{
+"title" : "正则表达式",
+"time" : "2016/05/28",
+"categories":"java@javascript",
+"url" : "/posts/2016/05/28/%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F.html"
+}
+
+,
+
+
+{
+"title" : "Java网络通信",
+"time" : "2016/05/27",
+"categories":"java",
+"url" : "/posts/2016/05/27/Java%E7%BD%91%E7%BB%9C%E9%80%9A%E4%BF%A1.html"
+}
+
+,
+
+
+{
+"title" : "Java注解(Annotation)学习",
+"time" : "2016/05/15",
+"categories":"java",
+"url" : "/posts/2016/05/15/java-%E6%B3%A8%E8%A7%A3-Annotation-%E5%AD%A6%E4%B9%A0.html"
+}
+
+,
+
+
+{
+"title" : "jQuery学习笔记",
+"time" : "2016/04/20",
+"categories":"javascript",
+"url" : "/posts/2016/04/20/jQuery%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0.html"
+}
+
+,
+
+
+{
+"title" : "flex布局笔记",
+"time" : "2016/04/18",
+"categories":"html/css",
+"url" : "/posts/2016/04/18/flex%E5%B8%83%E5%B1%80%E7%AC%94%E8%AE%B0.html"
+}
+
+,
+
+
+{
+"title" : "深入探究z-index",
+"time" : "2016/04/16",
+"categories":"html/css",
+"url" : "/posts/2016/04/16/%E6%B7%B1%E5%85%A5%E6%8E%A2%E7%A9%B6z-index.html"
+}
+
+,
+
+
+{
+"title" : "css的position定位",
+"time" : "2016/04/15",
+"categories":"html/css",
+"url" : "/posts/2016/04/15/css-position%E5%AD%A6%E4%B9%A0.html"
+}
+
+,
+
+
+{
+"title" : "用Virtualbox代替一键环境",
+"time" : "2016/03/28",
+"categories":"小技巧",
+"url" : "/posts/2016/03/28/%E7%94%A8Virtualbox%E4%BB%A3%E6%9B%BF%E4%B8%80%E9%94%AE%E7%8E%AF%E5%A2%83.html"
+}
+
+,
+
+
+{
+"title" : "CentOS6.3搭建LNMP环境",
+"time" : "2016/03/10",
+"categories":"linux@php",
+"url" : "/posts/2016/03/10/centos%E6%90%AD%E5%BB%BAlnmp%E7%8E%AF%E5%A2%83.html"
+}
+
+,
+
+
+{
+"title" : "腾讯云主机初始化",
+"time" : "2016/03/01",
+"categories":"linux",
+"url" : "/posts/2016/03/01/%E8%85%BE%E8%AE%AF%E4%BA%91%E4%B8%BB%E6%9C%BA%E5%88%9D%E5%A7%8B%E5%8C%96.html"
+}
+
+,
+
+
+{
+"title" : "主题预览",
+"time" : "2015/01/01",
+"categories":"jekyll",
+"url" : "/posts/2015/01/01/%E4%B8%BB%E9%A2%98%E9%A2%84%E8%A7%88.html"
+}
+
+
+]
+    var text_input = document.getElementById("text-input");
+    var searchlist = document.getElementById("searchlist");
+    var beforeinput = "";
+    
+    function addnode(json) {
+        var li = document.createElement('li');
+        var node = "<time>" + json.time + "&nbsp;</time>";
+        node += "<a href='" + json.url + "'>" + json.title + "</a>";
+        var arr = json.categories.split("@");
+        for (var j = 0; j < arr.length; j++) {
+            node += "<span>&nbsp;<a href='/pages/categories.html#" + arr[j] + "'>" + arr[j] + "</a></span>";
+        }
+        li.innerHTML = node;
+        searchlist.appendChild(li);
+    }
+
+    function inputchange() {
+        var currentinput = text_input.value;
+
+        //输入有变化，重新检索
+        if (beforeinput != currentinput.trim()) {
+            beforeinput = currentinput.trim();
+            if (beforeinput != "") {
+                searchlist.innerHTML = "";
+                var dataarray = data;
+                for (i = 0; i < dataarray.length; i++) {
+                    var keywords = beforeinput.split(" ");
+                    for (var j = 0; j < keywords.length; j++) {
+                        if (dataarray[i].time.match(keywords[j])) {
+                            addnode(dataarray[i]);
+                            break;
+                        }
+                        if (dataarray[i].title.toLocaleUpperCase().match(keywords[j].toLocaleUpperCase())) {
+                            addnode(dataarray[i]);
+                            break;
+                        }
+                        if (dataarray[i].categories.toLocaleUpperCase().match(keywords[j].toLocaleUpperCase())) {
+                            addnode(dataarray[i]);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                searchlist.innerHTML = "";
+            }
+        }
+    }
+
+    /*绑定事件*/
+    if (window.attachEvent) {
+        text_input.attachEvent("oninput", inputchange);
+    } else if (window.addEventListener) {
+        text_input.addEventListener("input", inputchange, false);
+    }
+}
+
+/*绑定事件*/
+if (window.attachEvent) {
+    window.attachEvent("onload", search);
+} else if (window.addEventListener) {
+    window.addEventListener("load", search, false);
+}
